f7ffc53f912a7d08c641f58e0ebf4e19ce806f93
George <blackwing460367@gmail.com>
2021-10-14 10:54:13 -0600

    String

--- C_Examples/Chapter1/fahrenheit2celsius.c
+++ C_Examples/Chapter1/fahrenheit2celsius.c
@@ -2,19 +2,19 @@
 
 /* print Fahrenheit-Celsius table
 	  for fahr = 0, 20, ..., 300 */
-int main() {
-	  int fahr, celsius;
-	  int lower, upper, step;
-	
-	  lower = 0;	/* lower limit of temperature table */
-	  upper = 300;	/* upper limit */
-	  step = 20;	/* step size */
 
-	  fahr = lower;
-	  while (fahr <= upper) {
-		  celsius = 5 * (fahr-32) / 9;
-		  printf("%d\t%d\n", fahr, celsius);
-		  fahr = fahr + step;
-	  }
-	  return(0);
+float convert(float temp){// Method that converts a float fahrenheit value to celsius
+
+	temp = 5 *(temp-32)/9;//Formula for conversion
+	return temp;//returns float value
 }
+
+int main() {
+	  float f = 98.34; // Fahrenheit value
+	  float converted_c; //Float that will contain the new celcius value
+	  converted_c = convert(f);//Calls convert method and assigns it to converted_c
+	  printf("Fahrenheit: %lf \n",f);//prints fahrenheit value
+	  printf("Celsius %lf \n", converted_c); // prints celsius value
+}
+
+
--- C_Examples/Chapter1/longest.c
+++ C_Examples/Chapter1/longest.c
@@ -30,7 +30,7 @@ int getaline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    for (i = 0;(c=getchar())!=EOF && c!='\n'; ++i)
 	s[i] = c;
     if (c == '\n') {
 	s[i] = c;
